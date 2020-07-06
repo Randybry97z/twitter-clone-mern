522ef0e6b2797c3657f675231de42c2a71077de8
Bryan <bssandovalrod@gmail.com>
2020-07-06 06:48:15 -0500

    Starting server

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+const app = require('./app')
+
+app.set('port', process.env.PORT || 4000)
+
+async function main() {
+  await app.listen(app.get('port'))
+  console.log(`Server on port ${app.get('port')}`)
+}
+
+main()
