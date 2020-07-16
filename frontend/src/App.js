c32d15742548ac6f56847cef56578fe94d0d9a77
Bryan <bssandovalrod@gmail.com>
2020-07-16 17:20:59 -0500

    Adding nav (unusable)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
+
+//styles
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+//Components
 import Index from './components/Index';
-import CreateUser from './components/User/CreateUser';
+import Signup from './components/User/Signup';
 import Login from './components/User/Login';
 import CreateTweet from './components/Tweet/CreateTweet';
 
@@ -12,7 +17,7 @@ function App() {
     <Router>
       <Route exact path="/" component={Index} />
       <Route exact path="/login" component={Login} />
-      <Route exact path="/users/create" component={CreateUser} />
+      <Route exact path="/signup" component={Signup} />
       <Route exact path="/tweet/create" component={CreateTweet} />
     </Router>
   );
--- frontend/src/components/Index.jsx
+++ frontend/src/components/Index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import Nav from './General/Nav'
 
 export default class Index extends Component {
   render() {
     return (
       <div>
+        <Nav></Nav>
         Index
-      </div>
+      </div >
     )
   }
 }
