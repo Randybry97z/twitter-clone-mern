ea843a5ad226f00cf4bad2b525df2a1f3ac06e82
Bryan <bssandovalrod@gmail.com>
2020-07-16 16:32:09 -0500

    adding router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Index from './components/Index';
 import CreateUser from './components/User/CreateUser';
 import Login from './components/User/Login';
+import CreateTweet from './components/Tweet/CreateTweet';
 
 function App() {
   return (
-    <div>
-      <Index />
-      <CreateUser />
-      <Login />
-      Hello World!
-    </div>
+    <Router>
+      <Route exact path="/" component={Index} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/users/create" component={CreateUser} />
+      <Route exact path="/tweet/create" component={CreateTweet} />
+    </Router>
   );
 }
 
